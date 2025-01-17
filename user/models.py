2eb010b020794e01ffb4bd5638aab92a8c7403a5
jinseo <jinseo040201@snu.ac.kr>
2025-01-17 14:35:28 +0900

    add: user model full_name column

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    full_name = models.CharField(max_length=128, blank=True)
 
     REQUIRED_FIELDS = []
     
